62f0235e1ce06482143d7b10d19f2c1bd32253ed
davidpaarup <107766727+davidpaarup@users.noreply.github.com>
2025-09-04 15:36:57 +0200

    Removed config cat

--- SemanticKernelApi/Plugins/TextToAudioPlugin.cs
+++ SemanticKernelApi/Plugins/TextToAudioPlugin.cs
@@ -1,35 +1,14 @@
 using System.ComponentModel;
 using Microsoft.SemanticKernel;
-using ConfigCat.Client;
 
 namespace SemanticKernelApi.Plugins;
 
-public class TextToAudioPlugin(IConfiguration configuration)
+public class TextToAudioPlugin()
 {
     [KernelFunction("convert_text_to_audio")]
     [Description("Converts the provided text to an audio and returns the bytes")]
-    private async Task<byte[]> ConvertTextToAudioAsync()
+    private byte[] ConvertTextToAudioAsync()
     {
-        var key = configuration["ConfigCatKey"];
-
-        if (key == null)
-        {
-            throw new Exception("Config cat configuration is missing");
-        }
-        
-        var client = ConfigCatClient.Get(key, options =>
-        {
-            options.PollingMode = PollingModes.LazyLoad();
-        });
-        
-        var isTextToAudioEnabled = await client.GetValueAsync("isTextToAudioEnabled", false);
-
-        if (!isTextToAudioEnabled)
-        {
-            throw new Exception("Text to audio conversion is disabled");
-        }
-
-        const string response = "This is some audio";
-        return System.Text.Encoding.UTF8.GetBytes(response);
+        throw new Exception("Text to audio conversion not implemented.");
     }
 }

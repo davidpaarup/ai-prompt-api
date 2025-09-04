03ee2da787ecb81a17320a9c3dcfdb24cf107409
davidpaarup <107766727+davidpaarup@users.noreply.github.com>
2025-09-04 15:15:20 +0200

    Forcing refresh

--- SemanticKernelApi/Plugins/TextToAudioPlugin.cs
+++ SemanticKernelApi/Plugins/TextToAudioPlugin.cs
@@ -18,6 +18,7 @@ public class TextToAudioPlugin(IConfiguration configuration)
         }
         
         var client = ConfigCatClient.Get(key);
+        await client.ForceRefreshAsync();
         
         var isTextToAudioEnabled = await client.GetValueAsync("isTextToAudioEnabled", false);
 
